651f9a7df77162db8595109125018a701880d184
stranger08 <ramanauskassaras@gmail.com>
2021-12-14 20:26:16 +0200

    Initial temperature graph

--- src/app/views/devices/device.component.ts
+++ src/app/views/devices/device.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 import { DevicesService } from '../../services/devices.service';
+import * as Chart from 'chart.js';
 
 @Component({
     templateUrl: 'device.component.html',
@@ -17,9 +18,52 @@ export class DeviceComponent {
 
   }
 
+  @ViewChild('chartRef')
+  private chartRef: ElementRef;
+  private chart: Chart;
+  public lineChartOptions: any = {
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: true,
+            color: '#dfe3e6',
+            zeroLineColor: '#dfe3e6'
+          },
+          ticks: {
+            callback: function(val, index) {
+              return `${val.split('T')[1].split('.')[0]}`
+            },  
+          }
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            display: true,
+            color: '#dfe3e6',
+            zeroLineColor: '#dfe3e6'
+          }
+        }
+      ]
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: false
+    },
+    legend: {
+      display: false
+    },
+  };
+
   private device:any = {};
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
     this.route.params.subscribe(params => {
       const ID = params['id'];
 
@@ -28,6 +72,18 @@ export class DeviceComponent {
 
         this.route.data.subscribe(data => {
           data.title = resp['name'];
+        });
+        console.log(this.device.data.map(d => {return {x: d.received, y: d.temp}}));
+
+        this.chart = new Chart(this.chartRef.nativeElement, {
+          type: 'line',
+          options: this.lineChartOptions,
+          
+          data: {
+            labels: this.device.data.map(d => d.received),
+            datasets: [
+            {data: this.device.data.map(d => d.temp), label: 'Temperature'},
+          ]}
         });
       });
     });
